1a4c5672cbdc2c744e5a98449cf1dca36ff3ea95
Max R <max.l.rauchman@gmail.com>
2022-12-16 10:37:42 -0500

    W3D5 Algos

--- DoublyLinkedList/index.js
+++ DoublyLinkedList/index.js
@@ -18,31 +18,126 @@ class DoublyLinkedList {
     }
 
     /**
-     * Creates a new node and adds it at the front of this list.
+     * Inserts a new node with the given newVal after the node that has the
+     * given targetVal as it's data.
      * - Time: O(?).
      * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
+     */
+    insertAfter(targetVal, newVal) {}
+
+    /**
+     * Inserts a new node with the given newVal before the node that has the
+     * given targetVal as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
+     */
+    insertBefore(targetVal, newVal) {}
+
+    /**
+     * Creates a new node and adds it at the front of this list.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
      * @param {any} data The data for the new node.
      * @returns {DoublyLinkedList} This list.
      */
-    insertAtFront(data) {}
+    insertAtFront(data) {
+        const newHead = new DLLNode(data);
+
+        if (this.isEmpty()) {
+            this.head = newHead;
+            this.tail = newHead;
+        } else {
+            const oldHead = this.head;
+            oldHead.prev = newHead;
+            newHead.next = oldHead;
+            this.head = newHead;
+        }
+        return this;
+    }
+
+    insertAtFront2(data) {
+        const newHead = new DLLNode(data);
+
+        if (this.isEmpty()) {
+            this.head = newHead;
+            this.tail = newHead;
+            return this;
+        }
+
+        this.head.prev = newHead;
+        newHead.next = this.head;
+        this.head = newHead;
+        return this;
+    }
 
     /**
      * Creates a new node and adds it at the back of this list.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(1) constant. No loop is needed since we have direct access to
+     *    the tail.
+     * - Space: O(1) constant.
      * @param {any} data The data for the new node.
      * @returns {DoublyLinkedList} This list.
      */
-    insertAtBack(data) {}
+    insertAtBack(data) {
+        const newTail = new DLLNode(data);
 
-    // EXTRA
+        if (this.isEmpty()) {
+            // if no head set the newTail to be both the head and the tail
+            this.head = newTail;
+            this.tail = newTail;
+        } else {
+            this.tail.next = newTail;
+            newTail.prev = this.tail;
+
+            this.tail = newTail;
+        }
+        return this;
+    }
+
     /**
      * Removes the middle node in this list.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(0.5n) -> O(n) linear, n = list length.
+     *    Since it's not constant we simplify it to O(n). Without the early
+     *    exists, it would not be 0.5n.
+     * - Space: O(1) constant.
      * @returns {any} The data of the removed node.
      */
-    removeMiddleNode() {}
+    removeMiddleNode() {
+        // when there is only 1 node, it is the middle, remove it.
+        if (!this.isEmpty() && this.head === this.tail) {
+            const removedData = this.head.data;
+            this.head = null;
+            this.tail = null;
+            return removedData;
+        }
+
+        let forwardRunner = this.head;
+        let backwardsRunner = this.tail;
+
+        while (forwardRunner && backwardsRunner) {
+            if (forwardRunner === backwardsRunner) {
+                const midNode = forwardRunner;
+                midNode.prev.next = midNode.next;
+                midNode.next.prev = midNode.prev;
+                return midNode.data;
+            }
+
+            // runners passed each other without stopping on the same node, even length, we can exit early
+            if (forwardRunner.prev === backwardsRunner) {
+                return null;
+            }
+
+            forwardRunner = forwardRunner.next;
+            backwardsRunner = backwardsRunner.prev;
+        }
+        return null;
+    }
 
     /**
      * Determines if this list is empty.
