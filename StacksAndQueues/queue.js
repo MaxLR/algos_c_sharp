16f6fba4a09194e51148eab44d84579ff2baf02d
Max R <max.l.rauchman@gmail.com>
2022-12-20 10:07:44 -0500

    Added LinkedList Class Definition

--- StacksAndQueues/queue.js
+++ StacksAndQueues/queue.js
@@ -52,3 +52,63 @@ class Queue {
 }
   
 /* EXTRA: Rebuild the above class using a linked list instead of an array. */
+
+/* 
+    In order to maintain an O(1) enqueue time complexity like .push with an array
+    We add a tail to our linked list so we can go directly to the last node
+*/
+
+class QueueNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+    }
+
+    class LinkedListQueue {
+    constructor() {
+        this.top = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    /**
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean} Indicates if the list is empty.
+     */
+    isEmpty() {}
+
+    /**
+     * Adds a given val to the back of the queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} val
+     * @returns {number} The new size of the queue.
+     */
+    enqueue(val) {}
+
+    /**
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item.
+     */
+    dequeue() {}
+
+    /**
+     * Retrieves the first item without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The first item.
+     */
+    front() {}
+
+    /**
+     * Determines if the given item is in the queue.
+     * - Time: O(n) linear.
+     * - Space: O(1) constant.
+     * @param {any} searchVal
+     * @returns {boolean}
+     */
+    contains(searchVal) {}
+}
