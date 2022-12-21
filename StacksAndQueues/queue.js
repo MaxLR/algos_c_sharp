5acef77fc9eaf55890bdf00008086be955c195e8
Max R <max.l.rauchman@gmail.com>
2022-12-21 10:27:58 -0500

    W4D3 Algos

--- StacksAndQueues/queue.js
+++ StacksAndQueues/queue.js
@@ -15,8 +15,11 @@ class Queue {
      * @param {any} item The new item to add to the back.
      * @returns {number} The new size of this queue.
      */
-    enqueue(item) {}
-  
+    enqueue(item) {
+        this.items.push(item);
+        return this.size();
+    }
+
     /**
      * Removes and returns the first item of this queue.
      * - Time: O(n) linear, due to having to shift all the remaining items to
@@ -24,31 +27,77 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    dequeue() {}
-  
+    dequeue() {
+        return this.items.shift();
+    }
+
     /**
      * Retrieves the first item without removing it.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    front() {}
-  
+    front() {
+        return this.items[0];
+    }
+
     /**
      * Returns whether or not this queue is empty.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {}
-  
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
     /**
      * Retrieves the size of this queue.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {number} The length.
      */
-    size() {}
+    size() {
+        return this.items.length;
+    }
+
+    print() {
+        const str = this.items.join(" ");
+        console.log(str);
+        return str;
+    }
+
+    /**
+   * Compares this queue to another given queue to see if they are equal.
+   * Avoid indexing the queue items directly via bracket notation, use the
+   * queue methods instead for practice.
+   * Use no extra array or objects.
+   * The queues should be returned to their original order when done.
+   * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+   *     array queue being O(n).
+   * - Space: O(1) constant.
+   * @param {Queue} q2 The queue to be compared against this queue.
+   * @returns {boolean} Whether all the items of the two queues are equal and
+   *    in the same order.
+   */
+    compareQueues(q2){
+        //Code goes here
+    }
+
+    /**
+   * Determines if the queue is a palindrome (same items forward and backwards).
+   * Avoid indexing queue items directly via bracket notation, instead use the
+   * queue methods for practice.
+   * Use only 1 stack as additional storage, no other arrays or objects.
+   * The queue should be returned to its original order when done.
+   * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+   *     array queue being O(n).
+   * - Space: O(n) from the stack being used to store the items again.
+   * @returns {boolean}
+   */
+    isPalindrome(){
+        //Code goes here
+    }
 }
   
 /* EXTRA: Rebuild the above class using a linked list instead of an array. */
@@ -63,22 +112,24 @@ class QueueNode {
         this.data = data;
         this.next = null;
     }
-    }
+}
 
-    class LinkedListQueue {
+class LinkedListQueue {
     constructor() {
-        this.top = null;
-        this.tail = null;
-        this.size = 0;
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
     }
-
+  
     /**
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {boolean} Indicates if the list is empty.
      */
-    isEmpty() {}
-
+    isEmpty() {
+      return this.head === null;
+    }
+  
     /**
      * Adds a given val to the back of the queue.
      * - Time: O(1) constant.
@@ -86,23 +137,52 @@ class QueueNode {
      * @param {any} val
      * @returns {number} The new size of the queue.
      */
-    enqueue(val) {}
-
+    enqueue(val) {
+        const newTail = new QueueNode(val);
+    
+        if (this.isEmpty()) {
+            this.head = newTail;
+            this.tail = newTail;
+        } else {
+            this.tail.next = newTail;
+            this.tail = newTail;
+        }
+        // pre-increment so the new size is returned otherwise old size is returned
+        return ++this.size;
+    }
+  
     /**
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {any} The removed item.
      */
-    dequeue() {}
-
+    dequeue() {
+        // remove head
+        if (!this.head) {
+            return null;
+        }
+    
+        const dequeued = this.head;
+        this.head = this.head.next;
+    
+        if (this.head === null) {
+            this.tail = null;
+        }
+    
+        this.size--;
+        return dequeued.data;
+    }
+  
     /**
      * Retrieves the first item without removing it.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {any} The first item.
      */
-    front() {}
-
+    front() {
+        return this.head ? this.head.data : null;
+    }
+  
     /**
      * Determines if the given item is in the queue.
      * - Time: O(n) linear.
@@ -110,5 +190,13 @@ class QueueNode {
      * @param {any} searchVal
      * @returns {boolean}
      */
-    contains(searchVal) {}
+    contains(searchVal) {
+        let runner = this.head;
+    
+        while (runner) {
+            if (runner.searchVal === searchVal) return true;
+            runner = runner.next;
+        }
+        return false;
+    }
 }
